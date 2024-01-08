fa553ca4ee04d901050e90918cae51c188e87d8d
Rodolphe <dutchr@live.fr>
2024-01-08 16:02:39 +0100

    :tada: Initial Commit
    💻Setup Mongo DATABASE

---
+++ src/index.js
@@ -0,0 +1,17 @@
+import express from "express";
+const app = express();
+import dotenv from "dotenv";
+dotenv.config();
+import mongoose from "mongoose";
+
+main().catch(err => console.log(err));
+
+async function main() {
+  await mongoose.connect(process.env.MONGO_URI);
+console.log(`[DATABASE] MONGODB is connected !!`)
+}
+const port = process.env.PORT;
+
+app.get("/", (req, res) => res.status(200).send("Welcome to the Social API"));
+
+app.listen(port, () => console.log(`listening on localhost:${port}`));
