c45512612006d5970482c5cb6568404e8b9590b3
Rodolphe <dutchr@live.fr>
2024-01-08 16:04:36 +0100

    MongoDB options

--- src/index.js
+++ src/index.js
@@ -4,11 +4,15 @@ import dotenv from "dotenv";
 dotenv.config();
 import mongoose from "mongoose";
 
-main().catch(err => console.log(err));
+main().catch((err) => console.log(err));
 
 async function main() {
-  await mongoose.connect(process.env.MONGO_URI);
-console.log(`[DATABASE] MONGODB is connected !!`)
+  await mongoose.connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    UseCreateIndex: true,
+    useUnifiedTopology: true,
+  });
+  console.log(`[DATABASE] MONGODB is connected !!`);
 }
 const port = process.env.PORT;
 
